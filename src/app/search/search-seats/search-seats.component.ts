f7720bc336a1bf8e357ecb3d9bbb6f189824922b
AlexandruBirta <alexbirta2011@gmail.com>
2023-05-24 20:38:17 +0300

    Search

---
+++ src/app/search/search-seats/search-seats.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'airlliant-search-seats',
+    templateUrl: './search-seats.component.html',
+    styleUrls: ['./search-seats.component.scss']
+})
+export class SearchSeatsComponent {
+
+}
